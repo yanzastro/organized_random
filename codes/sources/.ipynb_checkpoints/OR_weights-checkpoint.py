475c15c1bca8dc480c125d93c4cf9182a6a6da11
yanzastro <yanza.cosmo@gmail.com>
2022-09-16 15:48:07 +0000

    update files

--- codes/sources/.ipynb_checkpoints/OR_weights-checkpoint.py
+++ codes/sources/.ipynb_checkpoints/OR_weights-checkpoint.py
@@ -10,7 +10,7 @@ from tqdm import tqdm
 mpl.rcParams['figure.dpi'] = 200
 
 
-def calculate_or_weights(Ns, testing_radec, testing_som_ind):
+def calculate_or_weights(Ns, som_dim, testing_radec, testing_som_ind, testing_cluster_ind):
 
     '''
     This function calculates the organized random weight on the pixelized sky.
@@ -30,17 +30,21 @@ def calculate_or_weights(Ns, testing_radec, testing_som_ind):
     som_1dind_unique = np.unique(testing_som_ind)  # the unique SOM pixel indices (in 1D)
     delta_i_sum = 0
     weight_map = np.zeros(hp.nside2npix(Ns))
-    for som_ind in som_1dind_unique:
-        source_ind = np.where(testing_som_ind==som_ind)[0]  # pick out the catalog indices of sources that are in the som_ind'th SOM pixel
-        n_i = source_ind.size  # and the number of sources in that SOM pixel
+    number_contrast = np.zeros(som_dim**2)
+    
+    for cluster_ind in range(testing_cluster_ind.max()):
+        source_ind = np.where(testing_som_ind==som_ind)[0]  # pick out the catalog indices of sources that are in the cluster_ind'th hierarchical cluster
+        n_i = source_ind.size  # and the number of sources in that cluster
         hp_ind_ = hp_ind[source_ind]  # and the Healpix pixel indices
-        hp_ind_insomi_unique, n_p_i = np.unique(hp_ind_, return_counts=True)  # n_p_i is the number of sources that are in the i (som_ind here)-th SOM cell in each Healpix pixel
-        A_p_i = n_p_i/n_p[np.intersect1d(hp_ind_unique, hp_ind_insomi_unique, return_indices=True)[1]] * A_p  # the effective pixel area for the i-th SOM cell
+        hp_ind_incli_unique, n_p_i = np.unique(hp_ind_, return_counts=True)  # n_p_i is the number of sources that are in the i-th cluster in each Healpix pixel
+        A_p_i = n_p_i/n_p[np.intersect1d(hp_ind_unique, hp_ind_incli_unique, return_indices=True)[1]] * A_p  # the effective pixel area for the i-th SOM cell
         A_i = np.sum(A_p_i)  # the total effective area for the i-th SOM cell
         delta_i = n_i / A_i  # effective number density 
+        som_ind = testing_som_ind[testing_cluster_ind==cluster_ind]
+        number_contrast[som_ind] = delta_i
         delta_i_sum += delta_i
         weight_map[hp_ind_insomi_unique] += delta_i * n_p_i  # weight of each Healpix pixel
     
     weight_map/=np.mean(delta_i_sum)
     weight_map[weight_map==0] = hp.UNSEEN
-    return weight_map
+    return weight_map, number_contrast.reshape(som_dim, som_dim)
--- codes/sources/.ipynb_checkpoints/plot_som-checkpoint.py
+++ codes/sources/.ipynb_checkpoints/plot_som-checkpoint.py
@@ -13,7 +13,8 @@ mpl.rcParams['figure.dpi'] = 200
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
 
-def plot_som(ax, som_heatmap, topology='rectangular', colormap=cm.viridis, cbar_name=None):
+def plot_som(ax, som_heatmap, topology='rectangular', colormap=cm.viridis, cbar_name=None,
+            vmin=None, vmax=None):
     '''
     This function plots the pre-trained SOM.
     Input:
@@ -24,16 +25,17 @@ def plot_som(ax, som_heatmap, topology='rectangular', colormap=cm.viridis, cbar_
     colormap: the colormap to show the values. default: cm.viridis.
     cbar_name: the label on the color bar.
     '''
+    if vmin == None and vmax == None:
+        vmin = np.quantile(som_heatmap[~np.isnan(som_heatmap)],0.01)
+        vmax = np.quantile(som_heatmap[~np.isnan(som_heatmap)],0.99)
+    cscale = (som_heatmap-vmin) / (vmax - vmin)
+    som_dim = cscale.shape[0]
     if topology == 'rectangular':
         ax.matshow(som_heatmap.T, cmap=colormap, 
-                   vmin=np.quantile(som_heatmap[~np.isnan(som_heatmap)],0.01), 
-                   vmax=np.quantile(som_heatmap[~np.isnan(som_heatmap)], 0.99))
+                   vmin=vmin, 
+                   vmax=vmax)
     else:
-        smap_low = np.quantile(som_heatmap[~np.isnan(som_heatmap)],0.01)
-        smap_high = np.quantile(som_heatmap[~np.isnan(som_heatmap)],0.99)
-        
-        cscale = (som_heatmap-smap_low) / (smap_high-smap_low)
-        som_dim = cscale.shape[0]
+                
         yy, xx= np.meshgrid(np.arange(som_dim), np.arange(som_dim))
         shift = np.zeros(som_dim)
         shift[::2]=-0.5
@@ -41,7 +43,7 @@ def plot_som(ax, som_heatmap, topology='rectangular', colormap=cm.viridis, cbar_
         for i in range(cscale.shape[0]):
             for j in range(cscale.shape[1]):
                 wy = yy[(i, j)] * np.sqrt(3) / 2
-                if (cscale[i,j] == 0 or (np.isnan(cscale[i,j]))):
+                if np.isnan(cscale[i,j]):
                     color = 'k'
                 else:
                     color = colormap(cscale[i,j])
@@ -52,19 +54,18 @@ def plot_som(ax, som_heatmap, topology='rectangular', colormap=cm.viridis, cbar_
                                  facecolor=color, 
                                  edgecolor=color,
                                  #alpha=.4, 
-                                 lw=0,)
+                                 lw=0.2,)
                 ax.add_patch(hex)
 
-        scmap = plt.scatter([0,0],[0,0], s=0, c=[smap_low,
-                                                 smap_high], 
+    scmap = plt.scatter([0,0],[0,0], s=0, c=[vmin, vmax], 
                             cmap=colormap)
-        ax.set_xlim(-1,som_dim-.5)
-        ax.set_ylim(-0.5,som_dim * np.sqrt(3) / 2)
+    ax.set_xlim(-1,som_dim-.5)
+    ax.set_ylim(-0.5,som_dim * np.sqrt(3) / 2)
         
-        divider = make_axes_locatable(ax)
-        cax = divider.append_axes("right", size="5%", pad=0.05)
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
         
-        cb = plt.colorbar(scmap, cax=cax)
-        cb.ax.tick_params(labelsize=5)
-        cb.set_label(cbar_name, size='xx-small') 
-        ax.axis('off')
+    cb = plt.colorbar(scmap, cax=cax)
+    cb.ax.tick_params(labelsize=5)
+    cb.set_label(cbar_name, size='xx-small') 
+    ax.axis('off')
